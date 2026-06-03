6604e89586f3703f2dc15d670fb280b159375083
benja0901537987-beep <benja0901537987@gmail.com>
2026-06-03 23:51:10 +0300

    test: add test-keys.js to verify DeepSeek key pools and rotation

---
+++ test-keys.js
@@ -0,0 +1,4 @@
+import { getResponseDeepSeekKey, getLearningDeepSeekKey, getKeyStats } from './keys.js';
+console.log('Response key:', getResponseDeepSeekKey());
+console.log('Learning key:', getLearningDeepSeekKey());
+console.log('Key stats:', getKeyStats());
